917d73196700c0396629682a41e03fded89b8d21
SuttonKyle <suttonkylec@gmail.com>
2022-07-22 13:15:04 -0400

    fix tests

--- test/pdfViewer.test.ts
+++ test/pdfViewer.test.ts
@@ -14,21 +14,21 @@ describe('test pdf viewer', () => {
 
   it('should create PDF canvas container, canvas for every page, and skip pages', async () => {
     await renderPDF(containerDiv, documentUrl);
-    const canvasContainer = document.getElementsByClassName('canvasContainer');
+    const canvasContainer = document.getElementsByClassName('canvas-container');
     expect(canvasContainer.length).toEqual(1);
-    const canvasElements = document.getElementsByClassName('pdfViewerCanvas');
+    const canvasElements = document.getElementsByClassName('pdf-viewer-canvas');
     expect(canvasElements.length).toEqual(1);
-    const pageContainers = document.getElementsByClassName('pageContainer');
+    const pageContainers = document.getElementsByClassName('page-container');
     expect(pageContainers.length).toEqual(1);
-    const pageNumberInputs = document.getElementsByClassName('pageNumberInput');
+    const pageNumberInputs = document.getElementsByClassName('page-number-input');
     expect(pageNumberInputs.length).toEqual(1);
     const pageNumberInput = pageNumberInputs[0] as HTMLInputElement;
     expect(pageNumberInput.value).toEqual('1');
-    const nextButtons = document.getElementsByClassName('nextButton');
+    const nextButtons = document.getElementsByClassName('next-button');
     expect(nextButtons.length).toEqual(1);
-    const prevButtons = document.getElementsByClassName('prevButton');
+    const prevButtons = document.getElementsByClassName('prev-button');
     expect(prevButtons.length).toEqual(1);
-    const zoomButtons = document.getElementsByClassName('zoomButton');
+    const zoomButtons = document.getElementsByClassName('zoom-button');
     expect(zoomButtons.length).toEqual(1);
     const zoomButton = zoomButtons[0] as HTMLButtonElement;
     const firstPage = pageContainers[0] as HTMLDivElement;
